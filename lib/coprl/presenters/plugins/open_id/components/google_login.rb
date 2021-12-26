26d5496e0ed0c5eca3f7de95ca8fe32864e251fb
rhetprieto <rhetprieto@gmail.com>
2021-12-25 18:06:30 -0600

    Minor fixes for google login

--- lib/coprl/presenters/plugins/open_id/components/google_login.rb
+++ lib/coprl/presenters/plugins/open_id/components/google_login.rb
@@ -4,28 +4,32 @@ module Coprl
       module OpenId
         class GoogleLogin < DSL::Components::Base
 
-          attr_reader :google_client_id, :google_client_secret, :login_uri
+          attr_reader :google_client_id, :google_client_secret, :redirect_uri, :login_uri, :o_csrf_token
 
           def initialize(**attribs_, &block)
             super(type: :google_login, **attribs_, &block)
             @google_client_id = attribs.delete(:google_client_id) { ENV['GOOGLE_OAUTH_CLIENT_ID'] }
             @google_client_secret = attribs.delete(:google_client_secret) { ENV['GOOGLE_OAUTH_CLIENT_SECRET'] }
-            @login_uri = attribs.delete(:login_uri)
+            @redirect_uri = attribs.delete(:redirect_uri)
+            @o_csrf_token = attribs.delete(:o_csrf_token)
+            @login_uri = generate_connect_link
             expand!
           end
 
           private
 
           def generate_connect_link
-            # https://accounts.google.com/o/oauth2/v2/auth?
-            # response_type=code&
-            #   client_id=424911365001.apps.googleusercontent.com&
-            #     scope=openid%20email&
-            #   redirect_uri=https%3A//oauth2.example.com/code&
-            #   state=security_token%3D138r5719ru3e1%26url%3Dhttps%3A%2F%2Foauth2-login-demo.example.com%2FmyHome&
-            #   login_hint=jsmith@example.com&
-            #                      nonce=0394852-3190485-2490358&
-            #   hd=example.com
+            params = {
+              response_type: 'code',
+              client_id: google_client_id,
+              redirect_uri: redirect_uri,
+              scope: 'openid email',
+              state: o_csrf_token,
+              nonce: SecureRandom.hex(12),
+              access_type: 'offline'
+            }
+
+            "https://accounts.google.com/o/oauth2/v2/auth?#{params.to_query}"
           end
 
         end
